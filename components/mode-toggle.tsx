9b0aba8f99dbbeed4989676deb0df1a6b93a70b3
haoping6028 <lucas60303@gmail.com>
2025-02-28 14:58:14 +0800

    delete unused theme variable

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
