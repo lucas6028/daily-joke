e060eedc3013f7bc0990ff2d2a39de44b8dd90d5
haoping6028 <lucas60303@gmail.com>
2025-03-04 15:58:04 +0800

    fix bitwise error in hash

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     for (let i = 0; i < str.length; i++) {
       const char = str.charCodeAt(i)
       hash = (hash << 5) - hash + char
-      hash = hash & hash // Convert to 32bit integer
+      hash = hash | 0 // Convert to 32bit integer
     }
     return hash
   }
