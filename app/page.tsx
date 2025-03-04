4a99b6745f3a844eea803a42bfa538f753f20199
haoping6028 <lucas60303@gmail.com>
2025-03-04 14:21:31 +0800

    fetch today jokes from api

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,8 @@ import { Sparkles } from "lucide-react"
 import { motion } from "framer-motion"
 
 export default function Home() {
-  const { jokes } = useJokeContext()
+  const [jokes, setJokes] = useState()
   const [jokeOfTheDay, setJokeOfTheDay] = useState<Joke | null>(null)
-
-  // Get joke of the day based on current date
-  useEffect(() => {
-    if (jokes.length > 0) {
-      const date = new Date()
-      const dateString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-      const seed = hashCode(dateString)
-      const index = Math.abs(seed % jokes.length)
-      setJokeOfTheDay(jokes[index])
-    }
-  }, [jokes])
 
   // Simple hash function for consistent random selection based on date
   const hashCode = (str: string) => {
@@ -32,6 +21,48 @@ export default function Home() {
     }
     return hash
   }
+
+  // Get the hash index of today
+  const date = new Date()
+  const dateString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+  const seed = hashCode(dateString)
+  const index = Math.abs(seed % 20)
+
+  useEffect(() => {
+    if (jokes)
+      return
+
+    const fetchJokes = async () => {
+      try {
+
+        // Fetch todays joke
+        const res = await fetch(`http://localhost:3000/api/supabase/fetch-jokes?limit=${1}&id=${index}`)
+        const jokesFromDB = await res.json()
+        setJokes({...jokesFromDB, ratings: []})
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    fetchJokes()
+  }, [])
+
+  // Get joke of the day based on current date
+  useEffect(() => {
+    if (!jokes) return
+
+    const fetchRatings = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/api/supabase/fetch-ratings?id=${index}`)
+        const ratings = await res.json()
+        setJokeOfTheDay( {...jokes, ratings: ratings} )
+      } catch (err) {
+        console.error(err)
+      }
+    } 
+
+    fetchRatings()
+  }, [jokes])
 
   return (
     <div className="page-transition space-y-10">
