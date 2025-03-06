f7f065a59a456d3bd2f81b985b580bf7acaa43a4
haoping6028 <lucas60303@gmail.com>
2025-03-06 11:24:50 +0800

    delete debug console

--- app/api/supabase/fetch-jokes/route.ts
+++ app/api/supabase/fetch-jokes/route.ts
@@ -5,7 +5,6 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const limit = parseInt(searchParams.get('limit') || '1', 10)
   const id = parseInt(searchParams.get('id') || '1', 10)
-  console.log('id:', id)
 
   try {
     const {data: jokes, error } = await supabase
