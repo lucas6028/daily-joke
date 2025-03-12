1f19833b04a035ced1da62cfa2f1f090a31c83f8
haoping6028 <lucas60303@gmail.com>
2025-03-12 14:12:43 +0800

    add sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://daily-joke.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://daily-joke.vercel.app/random',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 0.8,
+    },
+    {
+      url: 'https://daily-joke.vercel.app/categories',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://daily-joke.vercel.app/favorites',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.7,
+    },
+  ]
+}
