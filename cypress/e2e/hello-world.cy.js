e7da9325b3b077b7b9172a5d03fd1f7ea4025516
haoping6028 <lucas60303@gmail.com>
2025-03-17 22:08:22 +0800

    check if the title is correct using Cypress

--- cypress/e2e/hello-world.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Hello World Test', () => {
-    it('should display the correct title', () => {
-        cy.visit('http://localhost:3000');
-        cy.title().should('include', 'Expected Title');
-    });
-});
