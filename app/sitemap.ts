40597ba69e24daf9663c302b99acd04fa3afb55f
haoping6028 <lucas60303@gmail.com>
2025-03-18 11:51:57 +0800

    add priority to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,19 +6,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://daily-joke.vercel.app",
       lastModified: new Date(),
       changeFrequency: "daily",
-      // priority: 1.0,
+      priority: 1.0,
     },
     {
       url: "https://daily-joke.vercel.app/random",
       lastModified: new Date(),
       changeFrequency: "always",
-      // priority: 0.8,
+      priority: 0.8,
     },
     {
       url: "https://daily-joke.vercel.app/categories",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      // priority: 0.8,
+      priority: 0.8,
     },
   ];
 }
