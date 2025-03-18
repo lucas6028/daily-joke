68a021ccdbe26f40d6d0aee3c8a07ee7ed552dab
haoping6028 <lucas60303@gmail.com>
2025-03-18 22:20:07 +0800

    add base url to cypress tests for github actions

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,8 @@
 describe("Daily Joke Title Test", () => {
   it("should display the correct title", () => {
-    cy.visit(process.env.NEXT_PUBLIC_BASE_URL);
+    cy.visit(
+      process.env.NEXT_PUBLIC_BASE_URL || "https://daily-joke.vercel.app"
+    );
     cy.title().should("include", "Daily Joke");
   });
 });
