7ec9fb08a7b83874f68bc4e9e570ead5b494e930
haoping6028 <lucas60303@gmail.com>
2025-03-18 22:01:30 +0800

    add defualt browser for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
   fixturesFolder: "cypress/fixtures",
+  defaultBrowser: "chrome",
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
