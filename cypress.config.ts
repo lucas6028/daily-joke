f30e3c4b6397f25f54b4dda1b21144c099f60624
haoping6028 <lucas60303@gmail.com>
2025-03-18 09:32:58 +0800

    delte import package in cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
-  fixturesFolder: 'cypress/fixtures',
+  fixturesFolder: "cypress/fixtures",
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    baseUrl: 'http://localhost:3000',
+    async setupNodeEvents(on, config) {},
+    baseUrl: "http://localhost:3000",
   },
-})
+});
