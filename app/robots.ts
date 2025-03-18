5dcb891bb3d8fa8e4365b5b24b07becd864433bd
haoping6028 <lucas60303@gmail.com>
2025-03-18 14:54:24 +0800

    modify robots.ts to all robots

--- app/robots.ts
+++ app/robots.ts
@@ -5,48 +5,10 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
-        disallow: ["/private/"],
-      },
-      {
-        userAgent: "Googlebot",
-        disallow: ["/admin/", "/tmp/"],
-        allow: ["/images/", "/css/", "/js/"],
-      },
-      {
-        userAgent: "Googlebot-Image",
-        allow: ["/images/"],
-      },
-      {
-        userAgent: "Bingbot",
-        disallow: ["/admin/", "/tmp/"],
-        allow: ["/images/", "/css/", "/js/"],
-      },
-      {
-        userAgent: "DuckDuckBot",
-        disallow: ["/admin/", "/tmp/"],
-        allow: ["/images/", "/css/", "/js/"],
-      },
-      {
-        userAgent: "Baiduspider",
-        disallow: "/",
-      },
-      {
-        userAgent: "OAI-SearchBot",
-        disallow: ["/admin/", "/tmp/"],
-        allow: ["/images/", "/css/", "/js/"],
-      },
-      {
-        userAgent: "ChatGPT-User",
-        disallow: ["/admin/", "/tmp/"],
-        allow: ["/images/", "/css/", "/js/"],
-      },
-      {
-        userAgent: "GPTBot",
-        disallow: ["/admin/", "/tmp/"],
-        allow: ["/images/", "/css/", "/js/"],
+        disallow: ["/private/", "/api/"],
       },
     ],
+    host: "https://daily-joke.vercel.app",
     sitemap: "https://daily-joke.vercel.app/sitemap.xml",
   };
 }
