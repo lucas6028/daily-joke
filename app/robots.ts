0795197818e13e09b4a60ec0dea8590aaa19fa52
haoping6028 <lucas60303@gmail.com>
2025-03-18 11:16:56 +0800

    change the format of robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,53 +1,52 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/private/'],
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/private/"],
       },
       {
-        userAgent: 'Googlebot',
-        disallow: ['/admin/', '/tmp/'],
-        allow: ['/images/', '/css/', '/js/'],
+        userAgent: "Googlebot",
+        disallow: ["/admin/", "/tmp/"],
+        allow: ["/images/", "/css/", "/js/"],
       },
       {
-        userAgent: 'Googlebot-Image',
-        allow: ['/images/'],
+        userAgent: "Googlebot-Image",
+        allow: ["/images/"],
       },
       {
-        userAgent: 'Bingbot',
-        disallow: ['/admin/', '/tmp/'],
-        allow: ['/images/', '/css/', '/js/'],
+        userAgent: "Bingbot",
+        disallow: ["/admin/", "/tmp/"],
+        allow: ["/images/", "/css/", "/js/"],
       },
       {
-        userAgent: 'DuckDuckBot',
-        disallow: ['/admin/', '/tmp/'],
-        allow: ['/images/', '/css/', '/js/'],
+        userAgent: "DuckDuckBot",
+        disallow: ["/admin/", "/tmp/"],
+        allow: ["/images/", "/css/", "/js/"],
       },
       {
-        userAgent: 'Baiduspider',
-        disallow: '/',
+        userAgent: "Baiduspider",
+        disallow: "/",
       },
       {
-        userAgent: 'OAI-SearchBot',
-        disallow: ['/admin/', '/tmp/'],
-        allow: ['/images/', '/css/', '/js/'],
+        userAgent: "OAI-SearchBot",
+        disallow: ["/admin/", "/tmp/"],
+        allow: ["/images/", "/css/", "/js/"],
       },
       {
-        userAgent: 'ChatGPT-User',
-        disallow: ['/admin/', '/tmp/'],
-        allow: ['/images/', '/css/', '/js/'],
+        userAgent: "ChatGPT-User",
+        disallow: ["/admin/", "/tmp/"],
+        allow: ["/images/", "/css/", "/js/"],
       },
       {
-        userAgent: 'GPTBot',
-        disallow: ['/admin/', '/tmp/'],
-        allow: ['/images/', '/css/', '/js/'],
+        userAgent: "GPTBot",
+        disallow: ["/admin/", "/tmp/"],
+        allow: ["/images/", "/css/", "/js/"],
       },
     ],
-    sitemap: 'https://daily-joke.vercel.app/sitemap.xml',
+    sitemap: "https://daily-joke.vercel.app/sitemap.xml",
   };
 }
-
