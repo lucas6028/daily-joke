cc7ead6d19a627e55b553326388922f7acc7643d
haoping6028 <lucas60303@gmail.com>
2025-03-20 18:04:07 +0800

    add configuration for cypress component testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,13 +5,22 @@ export default defineConfig({
   viewportHeight: 720,
   fixturesFolder: "cypress/fixtures",
   defaultBrowser: "chrome",
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     async setupNodeEvents(on, config) {},
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   },
+
   env: {
     BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
   },
 });
