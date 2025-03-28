c32d38ed64bd7323e9522a0f63978fc5e57656ad
haoping6028 <lucas60303@gmail.com>
2025-03-28 22:14:07 +0800

    add GET category joke api

---
+++ app/api/category/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { supabase } from '@/lib/supabase'
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const category = searchParams.get('category')
+  if (!category) {
+    return NextResponse.json({ message: 'Invalid category', status: 400 })
+  }
+
+  try {
+    const { data: jokes, error } = await supabase
+      .from('jokes')
+      .select(
+        `
+        *,
+        ratings:ratings(*)
+        `
+      )
+      .eq('category', category)
+
+    if (error) {
+      console.error(`Error while fetching ${category} jokes from supabase`, error)
+      return NextResponse.json({ message: error, statuts: 500 })
+    }
+
+    return NextResponse.json(jokes)
+  } catch (err) {
+    console.error(`Error while fetching ${category} jokes`, err)
+    return NextResponse.json({ message: err, status: 401 })
+  }
+}
