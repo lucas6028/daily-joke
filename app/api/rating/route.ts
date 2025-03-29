6ec0af0e9ad6fd59b512dc51572baaa7c4ed7afe
haoping6028 <lucas60303@gmail.com>
2025-03-29 10:28:14 +0800

    Fix: Fix typo & change status code
    Fix typo in error message
    Consider using status code 500 instead of 401 for database errors

--- app/api/rating/route.ts
+++ app/api/rating/route.ts
@@ -11,13 +11,13 @@ export async function GET(request: NextRequest) {
 
     if (error) {
       console.error('Error while fetching jokes from supabase', error)
-      return NextResponse.json({ message: 'Database error occured' }, { status: 500 })
+      return NextResponse.json({ message: 'Database error occurred' }, { status: 500 })
     }
 
     return NextResponse.json(ratings)
   } catch (err) {
     console.error('Error while fetching jokes', err)
-    return NextResponse.json({ message: 'Database error occurred.' }, { status: 401 })
+    return NextResponse.json({ message: 'Database error occurred.' }, { status: 500 })
   }
 }
 
@@ -44,7 +44,7 @@ export async function POST(request: NextRequest) {
 
     if (error) {
       console.error('Error while insert new rating from supabase', error)
-      return NextResponse.json({ message: 'Database error occured.' }, { status: 500 })
+      return NextResponse.json({ message: 'Database error occurred.' }, { status: 500 })
     }
 
     return NextResponse.json({
@@ -53,6 +53,6 @@ export async function POST(request: NextRequest) {
     })
   } catch (err) {
     console.error('Error while inserting new rating', err)
-    return NextResponse.json({ message: 'An unexpected error occurred' }, { status: 401 })
+    return NextResponse.json({ message: 'An unexpected error occurred' }, { status: 500 })
   }
 }
