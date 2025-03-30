967b06616c4c02d7699dfa3f4261204744301cd9
haoping6028 <lucas60303@gmail.com>
2025-03-30 18:11:27 +0800

    Add: add before each to each test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,11 +1,13 @@
 describe('Display correct layout in home page', () => {
-  it('should display correct title', () => {
+  beforeEach(() => {
     cy.visit(Cypress.env('home'))
+  })
+
+  it('should display correct title', () => {
     cy.contains('h1', '每天一則精選笑話').should('be.visible')
   })
 
   it('should display correct sub title', () => {
-    cy.visit(Cypress.env('home'))
     cy.contains('p', '帶給您歡樂與放鬆。讓您的一天從微笑開始！').should('be.visible')
   })
 })
