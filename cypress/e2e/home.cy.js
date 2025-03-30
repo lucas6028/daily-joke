fb6591d7b09d88ee273b1eafc8bfff1fcdbcd0b5
haoping6028 <lucas60303@gmail.com>
2025-03-30 19:52:59 +0800

    Add: add English documentation

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,10 +3,12 @@ describe('Display correct layout in home page', () => {
     cy.visit(Cypress.env('home'))
   })
 
+  // Validates the tilte text: "Joke of the Day"
   it('should display correct title', () => {
     cy.contains('h1', '每天一則精選笑話').should('be.visible')
   })
 
+  // Validates the subtitle text: "Bringing you joy and relaxation. Start your day with a smile!"
   it('should display correct sub title', () => {
     cy.contains('p', '帶給您歡樂與放鬆。讓您的一天從微笑開始！').should('be.visible')
   })
