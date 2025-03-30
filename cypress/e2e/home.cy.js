51f0b3b00fa4af68807169f8058e097d69ed08e8
haoping6028 <lucas60303@gmail.com>
2025-03-30 21:33:38 +0800

    Format: format home.cy.js

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -7,7 +7,7 @@ describe('Display correct layout in home page', () => {
   it('should display correct title', () => {
     cy.contains('h1', '每天一則精選笑話').should('be.visible')
   })
-  
+
   // Validates the subtitle text: "Bringing you joy and relaxation. Start your day with a smile!"
   it('should display correct sub title', () => {
     cy.contains('p', '帶給您歡樂與放鬆。讓您的一天從微笑開始！').should('be.visible')
