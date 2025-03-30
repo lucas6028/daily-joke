9630aca1eee520ff96678dc47adfb85ef5995b1b
haoping6028 <lucas60303@gmail.com>
2025-03-30 17:47:43 +0800

    Add: check layout of random page

--- cypress/e2e/random.cy.js
+++ cypress/e2e/random.cy.js
@@ -1,11 +1,11 @@
 describe('Display correct layout in random page', () => {
   it('should display correct title', () => {
     cy.visit(Cypress.env('random'))
-    cy.get('h1').should('exist').and('contain.text', '隨機笑話')
+    cy.contains('h1', '隨機笑話').should('be.visible')
   })
 
   it('should display correct sub title', () => {
     cy.visit(Cypress.env('random'))
-    cy.get('p').should('exist').and('contain.text', '點一下按鈕，獲取一個新的隨機笑話！')
+    cy.contains('p', '點一下按鈕，獲取一個新的隨機笑話！').should('be.visible')
   })
 })
