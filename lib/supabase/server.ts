15cc72d838d96239791c6080c60c23dee6fab681
haoping6028 <lucas60303@gmail.com>
2025-04-03 16:31:59 +0800

    Add: create a server supabase client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,6 +1,29 @@
-import { createClient } from '@supabase/supabase-js'
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string | ''
-const supabaseRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string | ''
+export async function createClient() {
+  const cookieStore = await cookies()
 
-export const supabase = createClient(supabaseUrl, supabaseRoleKey)
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  )
+}
