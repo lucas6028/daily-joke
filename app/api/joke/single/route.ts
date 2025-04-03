f17c05438b1d0689e6e8008abfe425f36c00348d
haoping6028 <lucas60303@gmail.com>
2025-04-03 10:36:51 +0800

    Modify: reorganize the directory of utils and libs

--- app/api/joke/single/route.ts
+++ app/api/joke/single/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { supabase } from '@/utils/supabase/client'
+import { supabase } from '@/libs/supabase/client'
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
