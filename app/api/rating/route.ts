f66adec1427de0e1b532e3efa01e00d91e8d08b9
haoping6028 <lucas60303@gmail.com>
2025-04-03 11:10:15 +0800

    Fix: modify the path of lib/supabase

--- app/api/rating/route.ts
+++ app/api/rating/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { supabase } from '@/libs/supabase/server'
+import { supabase } from '@/lib/supabase/server'
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
