1362e3e677a8243387b5b53282258480204dade0
haoping6028 <lucas60303@gmail.com>
2025-04-12 15:07:53 +0800

    Use an optional chain expression

--- utils/calculateAverage.ts
+++ utils/calculateAverage.ts
@@ -2,7 +2,7 @@ import { Joke } from '@/types/joke'
 import { Rating } from '@/types/rating'
 
 export function calculateJokeAverageRating(joke: Joke): number {
-  if (!joke || !joke.ratings || joke.ratings.length === 0) {
+  if (!joke?.ratings || joke.ratings.length === 0) {
     return 0
   }
   const totalRating = joke.ratings.reduce((acc: number, rating: Rating) => acc + rating.rating, 0)
