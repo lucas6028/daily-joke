e871e66ca8197cf70d8e01dfef7334e0ff4606b6
haoping6028 <lucas60303@gmail.com>
2025-04-30 20:32:26 +0800

    Delete redundant comment

--- app/categories/[category]/error.tsx
+++ app/categories/[category]/error.tsx
@@ -16,7 +16,6 @@ export default function CategoryError({
 }) {
   useEffect(() => {
     Sentry.captureException(error)
-    // Log the error to an error reporting service
     console.error(error)
   }, [error])
 
